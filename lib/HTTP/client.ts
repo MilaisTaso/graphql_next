e2669c0c2ca24cefdc3ec49eaf76f57722412619
MilaisTaso <livreblanche819@gmail.com>
2024-08-27 23:28:26 +0900

    http client作成

--- lib/HTTP/client.ts
+++ lib/HTTP/client.ts
@@ -1,33 +1,43 @@
-import { env } from "@/env/server";
-import ky from "ky";
-import { ENDPOINTS, type ApiEndpoint } from "@/lib/HTTP/endpoint";
-import { auth } from "@/auth";
+import ky from 'ky';
+import { HTTPError } from 'ky';
 
-const NON_AUTHORIZE_ENDPOINTS = ENDPOINTS
+import { env } from '@/env/server';
+import { ENDPOINTS, type ApiEndpoint } from '@/lib/HTTP/endpoint';
 
 class HttpClient {
   constructor(
-    private baseUrl : string = env.API_URL,
+    private baseUrl: string = env.API_URL,
+    
   ) {}
 
   private url(endpoint: ApiEndpoint) {
-    return `${this.baseUrl}/${endpoint}`
+    return `${this.baseUrl}/${endpoint}`;
   }
 
   private nonAuthorizeEndPoints = Object.values(ENDPOINTS)
-    .filter(value => value !== ENDPOINTS.GRAPHQL)
-    .map(value => this.url(value))
+    .filter((value) => value !== ENDPOINTS.GRAPHQL)
+    .map((value) => this.url(value));
 
   private api = ky.extend({
-    hooks: {
-      beforeRequest: [
-        
-        request => {
-          if (!this.nonAuthorizeEndPoints.includes(request.url)) {
-            request.headers.set('Authorization','aaa' )
-          }
-        }
-      ]
+    hooks: {},
+  });
+
+  protected async post<T>(endpoint: ApiEndpoint, body: any) {
+    try {
+      const response = await this.api.post(
+       this.url(endpoint),
+       {
+        json: body
+       }
+      )
+
+      if (response.status != 200) throw new Error(`HTTP Request Error status: ${response.status}`)
+    } catch (err) {
+      if (err instanceof HTTPError) {
+        throw err;
+      } else {
+        throw new Error(`Unexpected Error detail: ${err}`)
+      }
     }
-  })
+  }
 }
