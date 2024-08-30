007251c417e549887f03cb1759b3cd91feb3096a
tatsuki.onoda <tatsuki.onoda@arsaga.jp>
2024-08-30 16:36:37 +0900

    calenderコンポーネント追加

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,3 +1,23 @@
+'use client'
+
+import { useState } from 'react'
+
+import Calender from 'react-calendar'
+
+import { cn } from '@/lib/utils'
+
+type ValuePiece = Date | null
+
+type Value = ValuePiece | [ValuePiece, ValuePiece]
+
 export default function Todo() {
-  return <div>Todo Page</div>;
+  const [value, setValue] = useState<Value>(new Date())
+
+  return (
+    <div className={cn('my-2 flex flex-row flex-wrap items-start py-2')}>
+      <div className={cn('flex w-max grow basis-4 flex-col items-stretch pt-4')}>
+        <Calender onChange={setValue} value={value} className={cn('mx-auto')} locale='ja-JP'/>
+      </div>
+    </div>
+  )
 }
